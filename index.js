be2a92e83323f9ee368674c813e101d90230ad07
Debashish D. Dev <hello@debashish.dev>
2022-07-01 17:03:00 +0600

    Delete API

--- index.js
+++ index.js
@@ -59,6 +59,14 @@ async function run() {
       const result = await taskCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+    // delete task api
+    app.delete("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
